c36f3176f3d4fd47f8dd1a0290499c80a2779b03
Tobias Grothe <tobias-nicolai.grothe@fh-bielefeld.de>
2021-06-18 12:24:52 +0200

    add missing dep and a main class

---
+++ src/de/pmdungeon/desktop/Main.java
@@ -0,0 +1,10 @@
+package de.pmdungeon.desktop;
+
+import de.pmdungeon.game.Controller.MainController;
+
+public class Main extends MainController {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        DesktopLauncher.run(new Main());
+    }
+}
